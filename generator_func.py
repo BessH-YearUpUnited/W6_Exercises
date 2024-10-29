c4fc1376832a716d5b4833fb811994347ce308fb
elizabeas <bess@rcc.team>
2024-10-29 15:49:06 -0500

    uncomment code and remove extra bit from unrelated example

--- generator_func.py
+++ generator_func.py
@@ -1,17 +1,16 @@
-# # def calculator1(n1, n2):
-# #     return n1 + n2
+# def calculator1(n1, n2):
+#     return n1 + n2
 
-# # print(calculator1(34,25))
+# print(calculator1(34,25))
 
-# def calculator2(n1, n2):
-#     yield n1 + n2
-#     yield "Do you want to calculate another set of numbers?"
-#     y_n = input("Y or N").upper()
-#     yield f"You responded {y_n}"
+def calculator2(n1, n2):
+    yield n1 + n2
+    yield "Do you want to calculate another set of numbers?"
+    y_n = input("Y or N").upper()
+    yield f"You responded {y_n}"
 
 
-# for x in calculator2(17,8):
-#     print(x)
-
-for x in range(3): 
+for x in calculator2(17,8):
     print(x)
+
+
