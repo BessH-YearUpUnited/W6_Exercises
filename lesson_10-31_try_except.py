c14ad343cf19aee34f72d458fcfa9162b2a225ac
elizabeas <bess@rcc.team>
2024-10-31 14:13:08 -0500

    try_except examples

---
+++ lesson_10-31_try_except.py
@@ -0,0 +1,67 @@
+# try:
+#     list = []
+#     age = int(input("Please enter your age: "))
+#     try:
+#         list.append(age)
+#         print(list[23])
+#     except IndexError:
+#         print("Index out of range. Cannot locate specified item.")
+# except ValueError: 
+#     print("Error detected. Please input only a number for age.")
+# else:
+#     print(f"Age provided is: {age}")
+# finally:
+#     print("Thank you!")
+
+
+# try:
+#     f = open("survey_data.csv")
+#     data = ff.read()
+#     print(data[:100])
+# except:
+#     print("Error raised")
+# else: 
+#     print("File input accepted.")
+# finally:
+#     f.close()
+
+# print("Mission complete.")
+
+
+# print('# Division calculator # \n')
+
+# first_n = None
+
+# while first_n == None:
+#     try:
+#         first_n = float(input('First number: '))
+#     except:
+#         print('Invalid input. Please try again.')
+
+# second_n = None
+# answer = None
+
+# while second_n == None:
+#     try:
+#         second_n = float(input('Second number: '))
+#         answer = first_n / second_n
+#     except:
+#         pass
+#         second_n = None
+#     else:
+#         print(answer)
+
+# print("\n# Thank you for using the Divsion Calculator #")
+
+
+
+try:
+    boo = 90 / 1
+    result = bo
+except:
+    print("There is an error")
+else:
+    print("Program complete.")
+finally:
+    print("Have a nice day.")
+
